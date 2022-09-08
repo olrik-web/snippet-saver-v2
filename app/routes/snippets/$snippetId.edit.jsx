ef7c9de9d09fb0cc9a237a2561c3f23ba6b249c4
Marcus <molrik@outlook.com>
2022-09-08 16:31:37 +0200

    feat: UseLoaderData from data.json

--- app/routes/snippets/$snippetId.edit.jsx
+++ app/routes/snippets/$snippetId.edit.jsx
@@ -1,32 +1,54 @@
-import { redirect } from "@remix-run/node";
-import { Link, useParams } from "@remix-run/react";
+import { json, redirect } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import data from "~/data.json";
 
-// export async function action({ request }) {
-//   const body = await request.formData();
-//   const project = await editSnippet(body);
-//   return redirect(`/snippet/${snippetIds}`);
+export async function action({ request }) {
+  const body = await request.formData();
+  console.log(body);
+
+  return null;
+
+  // const postSnippit = await editSnippet(body);
+  // return redirect(`/snippet/${postSnippit.id}`);
+  return redirect("snippets/1");
+}
+
+// async function editSnippet(body) {
+//   console.log(body);
+//   return null;
 // }
 
+export function loader({ params, request }) {
+  const snippet = data.find((snippet) => snippet.id == params.snippetId);
+  return json(snippet);
+}
+
 export default function Edit() {
-  const { snippetId } = useParams();
+  const snippet = useLoaderData();
 
   return (
     <section>
       <article>
-        <h1 className="text-3xl font-bold">Edit page {snippetId}</h1>
-        <form method="post" action="/snippets">
+        <h1 className="text-3xl font-bold">Edit {snippet.title}</h1>
+        <Form method="put" action={`/snippets/${snippet.id}/edit`}>
           <label>
-            <input name="title" type="text" />
+            <input
+              name="title"
+              type="text"
+              defaultValue={snippet ? snippet.title : undefined}
+            />
           </label>
           <label>
-            <textarea name="description"></textarea>
+            <textarea
+              name="description"
+              defaultValue={snippet ? snippet.desc : undefined}
+            ></textarea>
           </label>
-          <button type="submit">Create</button>
-        </form>
+          <button type="submit">Update</button>
+        </Form>
         <code></code>
-        <Link to={`../${snippetId}`}>Cancel</Link>
+        <Link to={`../${snippet.id}`}>Cancel</Link>
       </article>
     </section>
   );
 }
-
--- app/routes/snippets/$snippetId.jsx
+++ app/routes/snippets/$snippetId.jsx
@@ -1,10 +1,17 @@
-import { Link, useParams } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData, useParams } from "@remix-run/react";
+import data from "~/data.json";
+
+export function loader({ params, request }) {
+  const snippet = data.find((snippet) => snippet.id == params.snippetId);
+  return json(snippet);
+}
 
 export default function Details() {
-  const { snippetId } = useParams();
+  const snippet = useLoaderData();
   return (
     <section>
-      <h1 className="text-3xl font-bold">Details page {snippetId}</h1>
+      <h1 className="text-3xl font-bold">Details {snippet.title}</h1>
       <code></code>
       <Link to="edit">Edit</Link>
     </section>
