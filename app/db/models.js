e7734804979889350c5a95f1ca8e532a6444c5b3
Marcus <molrik@outlook.com>
2022-09-09 17:49:11 +0200

    feat: small changes

---
+++ app/db/models.js
@@ -0,0 +1,39 @@
+import { mongoose } from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an email."],
+    unique: [true, "Email already exist."],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password."]
+  },
+  profile: {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const models = [
+  {
+    name: "User",
+    schema: userSchema,
+    collection: "User",
+  },
+];
